faf4735629a4bebf9ca465f7c8bc8c3212b0f19f
mauassenco <mau.assenco@gmail.com>
2025-02-11 16:43:46 -0300

    chore: adding theme proveider

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -3,9 +3,10 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
 
   * {
-    margin: 0;
+    margin: 0 auto;
     padding: 0;
     border: 0;
+    max-width: 1280px;
     font-size: 100%;
     vertical-align: baseline;
     box-sizing: border-box; 
